a67f25990de6d0407871590f745db2542ed89130
David Nowakowski <dnowakowski@neonomics.io>
2023-04-25 11:04:05 +0200

    feat: Initial commit

---
+++ src/test/java/com/dns/pactproducer/PactProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dns.pactproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PactProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
